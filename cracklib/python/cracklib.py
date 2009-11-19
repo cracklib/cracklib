350a5bb321a44333066bd9e8f1e76e315ccc87a0
Jan Dittberner <jan@dittberner.info>
2009-11-19 14:59:07 +0000

    update licensing and copyright information in Python extension
    git-svn-id: file:///tmp/cracklib-svn/trunk@151 4175fe1e-86d5-4fdc-8e6a-506fab9d8533

--- cracklib/python/cracklib.py
+++ cracklib/python/cracklib.py
@@ -4,23 +4,23 @@
 # Parts of this code are based on work Copyright (c) 2003 by Domenico
 # Andreoli.
 #
-# Copyright (c) 2008 Jan Dittberner <jan@dittberner.info>
+# Copyright (c) 2008, 2009 Jan Dittberner <jan@dittberner.info>
 #
 # This file is part of cracklib.
 #
-# cracklib is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
 #
-# cracklib is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU General Public License
-# along with Prua; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 """Python extensions for the cracklib binding.
 """
 
