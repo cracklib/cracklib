5ebc8dc5964c5c48edf3cb189548b7434f00cbdb
Mike Frysinger <vapier@gmail.com>
2010-10-03 19:29:39 +0000

    remove generated gettext files
    git-svn-id: file:///tmp/cracklib-svn/trunk@185 4175fe1e-86d5-4fdc-8e6a-506fab9d8533

--- cracklib/autogen.sh
+++ cracklib/autogen.sh
@@ -1,27 +1,6 @@
 #!/bin/sh -x
-
-# This script runs commands necessary to generate a Makefile for libgif.
-
-#echo "Warning: This script will run configure for you -- if you need to pass"
-#echo "  arguments to configure, please give them as arguments to this script."
-
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-THEDIR="`pwd`"
-cd $srcdir
-
-aclocal -I m4 || exit 1
-autoheader || exit 1
-libtoolize --automake --copy || exit 1
-automake --add-missing --copy || exit 1
-autoconf || exit 1
-automake || exit 1
-
-cd $THEDIR
-
-# I hate that... 
-#$srcdir/configure $*
-
-exit 0
-
+autopoint -f
+cd m4
+echo EXTRA_DIST = *.m4 > Makefile.am
+cd ..
+autoreconf -f -i
