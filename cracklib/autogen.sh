75b921b94c38f8f0cad27c13db196807e7f18bea
Nathan Neulinger <nneul@neulinger.org>
2013-12-06 13:51:48 +0000

    update for release
    git-svn-id: file:///tmp/cracklib-svn/trunk@225 4175fe1e-86d5-4fdc-8e6a-506fab9d8533

--- cracklib/autogen.sh
+++ cracklib/autogen.sh
@@ -4,3 +4,10 @@ cd m4
 echo EXTRA_DIST = *.m4 > Makefile.am
 cd ..
 autoreconf -f -i
+
+curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD" > config.guess
+curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD" > config.sub
+
+#for f in "lt~obsolete.m4" "ltversion.m4" "ltsugar.m4" "ltoptions.m4" "libtool.m4"; do
+	#curl "http://git.savannah.gnu.org/cgit/libtool.git/plain/m4/${f}?id=v2.4.2.418" > m4/${f}
+#done
